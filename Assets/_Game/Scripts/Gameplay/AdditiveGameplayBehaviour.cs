692ecdca1ba6ea9c614ecf448a798e5314d45b0a
Rodrigo Saguer Pacheco <49574554+Extelen@users.noreply.github.com>
2025-02-04 19:42:54 -0300

    FIC-427 Common Math Behaviour

--- Assets/_Game/Scripts/Gameplay/AdditiveGameplayBehaviour.cs
+++ Assets/_Game/Scripts/Gameplay/AdditiveGameplayBehaviour.cs
@@ -1,77 +1,27 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
-public class AdditiveGameplayBehaviour : GameplayBehaviour
+public class AdditiveGameplayBehaviour : CommonMathGameplay
 {
     // Variables
     [Header("Settings")]
     [SerializeField]
     private MinMaxInt m_additionRange = new MinMaxInt(0, 10);
 
-    [Header("References")]
-    [SerializeField]
-    private TMP_Text m_leftNumberRenderer;
-    [SerializeField]
-    private TMP_Text m_rightNumberRenderer;
-    [SerializeField]
-    private TMP_Text m_answerRenderer;
-
-    [Space]
-    [SerializeField]
-    private KeyboardBehaviour m_keyboardBehaviour;
-
-    private int m_currentNumber;
-    private int m_correctNumber;
-
     // Methods
-    /// <summary>
-    /// Awake is called when the script instance is being loaded.
-    /// </summary>
-    private void Awake()
+    public override int GetLeftNumber()
     {
-        m_currentNumber = -1;
-
-        int left = m_additionRange.Random();
-        m_leftNumberRenderer.text = left.ToString();
-
-        int right = m_additionRange.Random();
-        m_rightNumberRenderer.text = right.ToString();
-
-        m_correctNumber = left + right;
-        m_answerRenderer.text = "X";
+        return m_additionRange.Random();
     }
 
-    public override void StartGame()
+    public override int GetRightNumber()
     {
-        // Enable keyboard?
+        return m_additionRange.Random();
     }
 
-    public void OnStringChange(string str)
+    public override int GetCorrectNumber()
     {
-        if (string.IsNullOrEmpty(str))
-            return;
-
-        m_currentNumber = int.Parse(str);
-        m_answerRenderer.text = str;
-    }
-
-    private void ResetAnswer()
-    {
-        m_keyboardBehaviour.Clear();
-        m_answerRenderer.text = "X";
-    }
-
-    public void Check()
-    {
-        bool correct = m_currentNumber == m_correctNumber;
-
-        if (correct)
-            ModuleBehaviour.End();
-
-        else
-            ResetAnswer();
+        return LeftNumber + RightNumber;
     }
 }
---
+++ Assets/_Game/Scripts/Gameplay/CommonMathGameplay.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public abstract class CommonMathGameplay : GameplayBehaviour
+{
+    // Variables
+    [Header("References")]
+    [SerializeField]
+    private TMP_Text m_leftNumberRenderer;
+    [SerializeField]
+    private TMP_Text m_rightNumberRenderer;
+    [SerializeField]
+    private TMP_Text m_answerRenderer;
+
+    [Space]
+    [SerializeField]
+    private KeyboardBehaviour m_keyboardBehaviour;
+
+    protected int LeftNumber { get; set; }
+    protected int RightNumber { get; set; }
+
+    private int m_currentNumber;
+    private int m_correctNumber;
+
+    // Methods
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    private void Awake()
+    {
+        m_currentNumber = -1;
+
+        LeftNumber = GetLeftNumber();
+        RightNumber = GetRightNumber();
+        m_correctNumber = GetCorrectNumber();
+
+        m_leftNumberRenderer.text = LeftNumber.ToString();
+        m_rightNumberRenderer.text = RightNumber.ToString();
+        m_answerRenderer.text = "X";
+    }
+
+    public override void StartGame()
+    {
+        // Enable keyboard?
+    }
+
+    public abstract int GetLeftNumber();
+    public abstract int GetRightNumber();
+    public abstract int GetCorrectNumber();
+
+    public void OnStringChange(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return;
+
+        m_currentNumber = int.Parse(str);
+        m_answerRenderer.text = str;
+    }
+
+    private void ResetAnswer()
+    {
+        m_keyboardBehaviour.Clear();
+        m_answerRenderer.text = "X";
+    }
+
+    public void Check()
+    {
+        bool correct = m_currentNumber == m_correctNumber;
+
+        if (correct)
+            ModuleBehaviour.End();
+
+        else
+            ResetAnswer();
+    }
+}
